ca142ba6418b69e6801fe5b1129bdcc1c027f0af
Diego-ballesteros-fdz <diego.ball.fdz@gmail.com>
2026-05-25 19:08:07 +0200

    FIX errors

--- blank/packages/app/api/index.ts
+++ blank/packages/app/api/index.ts
@@ -14,7 +14,7 @@ const getBaseUrl = () => {
 
 export const api = axios.create({
   // baseURL: getBaseUrl(),
-  baseURL: 'https://54.160.126.242:8080/api/v1',
+  baseURL: 'http://54.160.126.242:8080/api/v1',
   timeout: 10000, //tiempo límite de la llamada
   //headers de las llamadas
   headers: {
